f6c1adc757295842728d00d9753b4fda85dcdde5
Anastasiya-Vavilova <ntttru@bk.ru>
2024-12-19 00:31:37 +0300

    Первое задача в первом задании полностью готова

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,25 @@
 "use strict"
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+
+	let disc = Math.pow(b, 2) - 4 * a * c;
+	if (disc < 0) {
+
+	}
+
+	if (disc === 0) {
+		let x = -b / (2 * a);
+		arr.push(x);
+	}
+
+	if (disc > 0) {
+		let x1 = (-b + Math.sqrt(disc)) / (2 * a);
+		let x2 = (-b - Math.sqrt(disc)) / (2 * a);
+		arr.push(x1, x2)
+	}
+
+	return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
